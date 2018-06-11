4fc8568ba339f1dea785be5c67db79728c314094
Molyn Eriksson <molyneriksson@gmail.com>
2018-06-11 16:21:52 +0200

    database seeded

--- LabT2Dependency/Data/DbInitializer.cs
+++ LabT2Dependency/Data/DbInitializer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LabT2Dependency.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +11,40 @@ namespace LabT2Dependency.Data
         public static void Initialize(TravelContext context)
         {
             context.Database.EnsureCreated();
+
+            if (context.Journeys.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            List<Journey> journeys = new List<Journey>
+            {
+                new Journey(){StartPosition="Gothenburg", EndPosition="Stockholm", Distance=470.3}
+            };
+     
+
+            foreach (var j in journeys)
+            {
+                context.Journeys.Add(j);
+            }
+
+            List<Journey> journeysCustomer1 = new List<Journey>
+            {
+                journeys[0]
+            };
+
+            var customers = new Models.Costumer[]
+            {
+                new Models.Costumer{ Name="Molyn", Age=26, Occupation="Programmer", Journeys = journeysCustomer1}
+            };
+
+            foreach (var c in customers)
+            {
+                context.Costumers.Add(c);
+            }
+
+            context.SaveChanges();
+            
         }
     }
 }
